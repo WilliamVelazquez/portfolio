ec25b9fa2602701c8085486bd6190bc08bf377d3
William Velazquez <william.velazquez.a@gmail.com>
2018-12-05 17:50:56 -0600

    Adding env-config File

--- next.config.js
+++ next.config.js
@@ -58,9 +58,9 @@ module.exports = withOffline({
       }
     ]
   },
-  //assetPrefix: process.env.NODE_ENV === 'production' ? 'https://williamvelazquez.github.io/portfolio' : ''
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://williamvelazquez.github.io/portfolio' : ''
   //assetPrefix: process.env.NODE_ENV === 'production' ? '/{portfolio}' : ''
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : ''
+  //assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : ''
   //Provide own no-op service worker in development
   //Useful to test web push notifications in development
   //devSwSrc: '/path/to/my/dev/service-worker.js'
