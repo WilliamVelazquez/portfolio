79e4a5f70579f811e08d572f7e6feed63d477ead
William Velazquez <william.velazquez.a@gmail.com>
2019-11-28 21:21:33 -0600

    Updating AboutMusic Component Content

--- components/sections/about/AboutMusic.js
+++ components/sections/about/AboutMusic.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import Gallery from '../../../containers/gallery/Gallery';
 
+const MUSIC_IMG_0 = '/static/music/music_0.png';
+const MUSIC_IMG_1 = '/static/music/music_1.png';
+const MUSIC_IMG_2 = '/static/music/music_2.png';
+const MUSIC_IMG_3 = '/static/music/music_3.png';
+const MUSIC_IMG_4 = '/static/music/music_4.png';
+const MUSIC_IMG_5 = '/static/music/music_5.png';
+
+const MUSIC_IMAGES = [
+  { id: 1, src: MUSIC_IMG_0 },
+  { id: 2, src: MUSIC_IMG_1 },
+  { id: 3, src: MUSIC_IMG_2 },
+  { id: 4, src: MUSIC_IMG_3 },
+  { id: 5, src: MUSIC_IMG_4 },
+  { id: 6, src: MUSIC_IMG_5 }
+];
+
 function AboutMusic(props) {
-  const IMAGES = [
-    { id: 1, src: 'https://source.unsplash.com/PC_lbSSxCZE/800x600' },
-    { id: 2, src: 'https://source.unsplash.com/lVmR1YaBGG4/800x600'},
-    { id: 3, src: 'https://source.unsplash.com/5KvPQc1Uklk/800x600'},
-    { id: 4, src: 'https://source.unsplash.com/GtYFwFrFbMA/800x600'},
-    { id: 5, src: 'https://source.unsplash.com/Igct8iZucFI/800x600'},
-    { id: 6, src: 'https://source.unsplash.com/M01DfkOqz7I/800x600'},
-    { id: 7, src: 'https://source.unsplash.com/MoI_cHNcSK8/800x600'},
-    { id: 8, src: 'https://source.unsplash.com/M0WbGFRTXqU/800x600'}
-  ];
   return(
     <React.Fragment>
       <p className="section-text">I used to play the Saxophone 🎷 and the Oboe on a Symphonic Orchestra 🎶 some years ago. I had the opportunity to play as an oboe soloist in an important place in my country with the best art school.</p>
       <p className="section-text">I love playing different musical instruments like the Piano 🎹, the Transverse Flute and I'm learning to play the Guitar 🎸 and the Drums🥁.</p>
       {/* <p className="section-text">I love to play musical instruments such as the Saxophone and the Oboe.🎵</p> */} 
       <Gallery 
-        columns={8}
+        columns={6}
         imageGap="0px"
-        images={IMAGES}
+        images={MUSIC_IMAGES}
         maxWidth={false}
-        tabletColumns={6}
-        mobileColumns={4}
+        tabletColumns={3}
+        mobileColumns={3}
         overlayBackgroundColor="transparent"
       />
 			<style jsx>{`
